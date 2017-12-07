ad64a6e8dec87a3e310f3c6cfbbf7c5021d1e338
Ralph Quapp <rhquapp@gmail.com>
2017-12-07 14:37:47 -0500

    move specs directory

--- src/main/g8/$module1__normalized$/src/test/scala/$package__packaged$/$module1__normalize$/$module1__Camel$Spec.scala
+++ src/main/g8/$module1__normalized$/src/test/scala/$package__packaged$/$module1__normalize$/$module1__Camel$Spec.scala
@@ -1,6 +1,6 @@
 package $package$.$module1;format="word"$
 
-import specs._
+import $package$.specs._
 
 class $module1;format="Camel"$Spec extends UnitTestSpec {
   "Hello" should "have tests" in {
