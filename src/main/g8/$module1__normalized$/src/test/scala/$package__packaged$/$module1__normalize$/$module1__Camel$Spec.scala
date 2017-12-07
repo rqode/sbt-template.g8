609d475a840d2b4d3809b1a7618ac10507065ff0
Ralph Quapp <rhquapp@gmail.com>
2017-12-06 22:40:52 -0500

    add unit test base

---
+++ src/main/g8/$module1__normalized$/src/test/scala/$package__packaged$/$module1__normalize$/$module1__Camel$Spec.scala
@@ -0,0 +1,10 @@
+package $package$.$module1;format="word"$
+
+import specs._
+
+class $module1;format="Camel"$Spec extends UnitTestSpec {
+  "Hello" should "have tests" in {
+    true should be === true
+  }
+
+}
---
+++ src/main/g8/$module1__normalized$/src/test/scala/specs/UnitTestSpec.scala
@@ -0,0 +1,5 @@
+package specs
+
+import org.scalatest._
+
+abstract class UnitTestSpec extends FlatSpec with Matchers
